064179552a7a33c1a52fd7f015cba83f5448aee1
James Lu <jamtlu@gmail.com>
2018-02-04 16:07:00 -0500

    Preempt transition

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -5,7 +5,10 @@ export class MainScreen extends React.Component {
     // in an actual implementation, there would be some kind of debounce
     render() {
         return (
-            <Button title={"Go"} onPress={() => this.props.navigation.navigate("SlaveScreen")}/>
+            <Button title={"Go"} onPress={() => {
+                db.setTransitionEnded(true)
+                this.props.navigation.navigate("SlaveScreen")
+            }}/>
         )
     }
 }
